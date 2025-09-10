a56afa6c275d72e0babc6f245efb3eec8e5acf22
vergel <vergelsantiago0509@gmail.com>
2025-09-10 09:11:50 +0800

    Add files via upload

---
+++ fundamentals-part-04/04-guess-my-number-game/04-guess-my-number-game/starter/script.js
@@ -0,0 +1,75 @@
+'use strict';
+
+console.log('Game setup ready');
+
+//===== DOM Element Selection - The Foundation
+console.log('=== DOM ELEMENT SELECTION ===');
+
+const messageEl = document.querySelector('.message');
+console.log(messageEl);
+
+console.log(messageEl.textContent);
+
+//messageEl.textContent = 'Hello from JavaScript!';
+
+//Access score class name
+
+const scoreEl = document.querySelector('.score');
+
+console.log('Score element:', scoreEl);
+
+const numberEl = document.querySelector('.number');
+//numberEl.textContent = 15;
+
+const highscoreEl = document.querySelector('.highscore');
+
+console.log('Current highscore:', highscoreEl.text);
+
+const guessEl = document.querySelector('.guess');
+
+//guessEl.value = 10;
+
+console.log('Guess input value:', guessEl.value);
+
+// Game state variables
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+console.log('Secret number:', secretNumber);
+
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.score').textContent = score;
+document.querySelector('.highscore').textContent = highscore;
+
+console.log('Game state intiialized');
+
+let playerName = 'Vergel';
+let attempts = 0;
+let gameActive = true;
+
+console.log('Player:', playerName);
+console.log('Attempts:', attempts);
+console.log('Game active:', gameActive);
+
+// Basic Game Logic
+
+document.querySelector('.check').addEventListener('click', function () {
+    console.log('Check button clicked')
+
+const guess = Number(document.querySelector('.guess').value);
+console.log('Player guess:', guess);
+
+if (guess === secretNumber) {
+    console.log('Correct guess!');
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+} else if (guess > secretNumber) {
+    console.log('Too high!');
+    document.querySelector('.message').textContent = 'Too high!';
+} else if (guess < secretNumber) {
+    console.log('Too low');
+    document.querySelector('.message').textContent = 'Too low!';
+} 
+});
