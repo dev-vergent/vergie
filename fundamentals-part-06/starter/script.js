1f933e4c03ebe988b8b4c298b01524d3f96bac9e
dev-vergent <vergelsantiago0509@gmail.com>
2025-09-28 22:12:02 +0800

    06

--- fundamentals-part-06/starter/script.js
+++ fundamentals-part-06/starter/script.js
@@ -1,36 +1,28 @@
-'use strict';
+const btnRoll = document.querySelector('.btn--roll');
 
-// Game variables
-let scores, currentScore, activePlayer, playing;
-
-// Select elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-
-// Start/reset game
-const init = function () {
-  scores = [0, 0];
+// Switch player function
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
-  activePlayer = 0;
-  playing = true;
-
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  diceEl.classList.add('hidden');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
 };
 
-init();
-btnNew.addEventListener('click', init);
+// Roll dice button
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
