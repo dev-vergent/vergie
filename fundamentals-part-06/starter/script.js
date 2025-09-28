ac7e0ce42f1c3ee7c688fad60728f5fe792bdf7b
dev-vergent <vergelsantiago0509@gmail.com>
2025-09-28 22:11:03 +0800

    06

---
+++ fundamentals-part-06/starter/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Game variables
+let scores, currentScore, activePlayer, playing;
+
+// Select elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+
+// Start/reset game
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+};
+
+init();
+btnNew.addEventListener('click', init);
